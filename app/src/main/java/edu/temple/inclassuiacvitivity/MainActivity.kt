8b9b867d1cb2ebaf14ca6b1d331a69fcdcd64e54
stan39049 <stan39049@gmail.com>
2025-02-04 11:47:33 -0500

    Added constraints to layout and made an array adapter for the spinner

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         val numberArray = arrayOf(5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100)
 
         /* TODO Step 3: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+        spinner.adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_dropdown_item_1line,numberArray)
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
